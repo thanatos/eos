f539303caec867c23d50ff552e509df08e8f98d7
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2016-07-23 18:19:20 -0700

    Initial commit of a hobby OS, tentatively named "Eos".

---
+++ amd64/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+x86_64-elf-as multiboot-header.S -o multiboot-header.o
+x86_64-elf-as entry.S -o entry.o
+x86_64-elf-as text-ops.S -o text-ops.o
+
+x86_64-elf-ld \
+	--nmagic \
+	-o kernel.bin \
+	-T eos.ld \
+	multiboot-header.o entry.o
---
+++ amd64/mkimage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d isofiles ]]; then
+	rm -rf isofiles
+fi
+
+# Stage the data we want in our image:
+mkdir -p isofiles/boot/grub
+cp grub.cfg isofiles/boot/grub/.
+cp kernel.bin isofiles/boot/.
+
+# Make the image:
+grub2-mkrescue -o os.iso isofiles
+
+# Clean up:
+rm -rf isofiles
