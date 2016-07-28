fd8c91fdacb3cdc6879191ff100513b8474250a6
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2016-07-27 22:41:51 -0700

    Do a bunch of pre-launch checks for 64-bit mode.
    Long mode (64 bits) requires a bunch of checks to make sure the CPU
    supports it.

--- amd64/build.sh
+++ amd64/build.sh
@@ -5,9 +5,10 @@ set -ex
 x86_64-elf-as multiboot-header.S -o multiboot-header.o
 x86_64-elf-as entry.S -o entry.o
 x86_64-elf-as text-ops.S -o text-ops.o
+x86_64-elf-as boot-checks.S -o boot-checks.o
 
 x86_64-elf-ld \
 	--nmagic \
 	-o kernel.bin \
 	-T eos.ld \
-	multiboot-header.o entry.o text-ops.o
+	multiboot-header.o entry.o text-ops.o boot-checks.o
