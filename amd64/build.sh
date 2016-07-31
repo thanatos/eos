0fedca791ecc67eab6c556b1437476f3a65a0a37
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2016-07-31 13:47:45 -0700

    Enable paging and long mode!

--- amd64/build.sh
+++ amd64/build.sh
@@ -6,9 +6,10 @@ x86_64-elf-as multiboot-header.S -o multiboot-header.o
 x86_64-elf-as entry.S -o entry.o
 x86_64-elf-as text-ops.S -o text-ops.o
 x86_64-elf-as boot-checks.S -o boot-checks.o
+x86_64-elf-as long-mode.S -o long-mode.o
 
 x86_64-elf-ld \
 	--nmagic \
 	-o kernel.bin \
 	-T eos.ld \
-	multiboot-header.o entry.o text-ops.o boot-checks.o
+	multiboot-header.o entry.o text-ops.o boot-checks.o long-mode.o
