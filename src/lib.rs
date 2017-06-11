0219e083176726febcc39e2f0026c53c46968109
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2017-06-10 21:12:47 -0700

    Remove most of the tutorial code from lib.rs
    We've since done the TODO: we essentially have a better, more Rust-y VGA text
    printing system, so use that.
    
    Update LICENSE, since we'd removed the floating point patch and the note about
    the tutorial code no longer applies, since we removed the tutorial code.

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,6 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-
-    /* Most of the contents of this function are from
-     *   http://os.phil-opp.com/set-up-rust.html
-     * This code, because it comes from his blog, carries the license from that
-     * blog (MIT or Apache License 2.0)
-     * TODO: write some real VGA handling code.
-     */
-
-    // ATTENTION: we have a very small stack and no guard page
-
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the VGA buffer.
-    let buffer_ptr = (0xb8000 + 80 * 2 * 5) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
     vga_text::init();
     vga_text::write_string("This is a test of VGA console writing.\n", 0xa);
     panic!("Reached the end of rust_main()");
